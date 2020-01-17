342dcf918ee3cb3256a63ced98b97e4e294978da
Kevin Quinn <kevinquinn1918@gmail.com>
2020-01-17 18:17:59 -0500

    Fixed bug in messages

--- client/src/components/layouts/games/MessageItem.js
+++ client/src/components/layouts/games/MessageItem.js
@@ -18,7 +18,7 @@ const MessageItem = ({ message, id }) => {
       <tr key={id} className="f-xs">
         <td>{convertDate(message.createdAt)}</td>
         {/* <td>{message.user.name.substring(0, 12)}</td> */}
-        <td>To be fixed</td>
+        <td>{message.user.name}</td>
         <td className="text-left">{message.gameMessage}</td>
       </tr>
     </Fragment>
